a146fb06d7c9039a6e6d82922f04a9c1ae521341
LinhNC <nclinh184@gmail.com>
2021-07-18 00:38:23 +0700

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <main>
+        <p>Mổ cò từ 2 ngón đến 10 ngón</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
